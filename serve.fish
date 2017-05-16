67bcb065c88e34bc8a5b57722fc3add8151e8769
Philip Thrasher <philip.thrasher@cybergrx.com>
2017-05-16 10:16:26 -0400

    warn properly

--- serve.fish
+++ serve.fish
@@ -1,5 +1,8 @@
 function serve
   if command --search http-server >/dev/null do
     http-server . -p 8000 -a 0.0.0.0 -i
+  else
+    echo "http-server not installed"
+    echo "  npm i -g http-server"
   end
 end
